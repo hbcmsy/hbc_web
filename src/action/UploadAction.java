98e23b2ec0ecb5b65a947cb0d595d0ab36267265
yangyaofei <yangyaofei@gmail.com>
2014-08-19 16:01:56 +0800

    debug
    Signed-off-by: yangyaofei <yangyaofei@gmail.com>

--- src/action/UploadAction.java
+++ src/action/UploadAction.java
@@ -85,41 +85,43 @@ public class UploadAction extends ActionSupport{
     public String image() {
     	Properties props=System.getProperties();
     	String separator = props.getProperty("file.separator");
-    	System.err.print("execute start");
+    	System.err.printf("execute start");
         InputStream is = null;
         try {
             HttpServletRequest request = ServletActionContext.getRequest();
             article_ID =(int)request.getSession().getAttribute("article_ID");
             is = new FileInputStream(upload);
             String uploadPath = ServletActionContext.getServletContext().getRealPath("articleImage")+separator+String.valueOf(article_ID);
-            System.err.print("uploadPath:"+uploadPath);
+            System.err.printf("uploadPath:"+uploadPath);
             String fileName = java.util.UUID.randomUUID().toString(); 
             File dir = new File(uploadPath);
-            System.err.print("准备创建文件夹");
+            System.err.printf("准备创建文件夹");
             if(!dir.exists())
                 dir.mkdir();
-            System.err.print("创建目录完成");
+            System.err.printf("创建目录完成");
             fileName += uploadFileName.substring(uploadFileName.length() - 4);
             File toFile = new File(uploadPath+separator+fileName);
             if(!toFile.exists())
                 toFile.createNewFile();
-            System.err.print("创建文件完成");
+            System.err.printf("创建文件完成");
             OutputStream os = new FileOutputStream(toFile);
             byte[] buffer = new byte[1024];
             int length = 0;
             while ((length = is.read(buffer)) > 0) {
                 os.write(buffer, 0, length);
-            }   is.close();
-            os.close();
+            }
+            System.err.printf("写入完成");
             is.close();
-            HttpServletResponse response = ServletActionContext.getResponse();  
+            os.close();
+            System.err.printf("回写");
+            HttpServletResponse response = ServletActionContext.getResponse();
             response.setCharacterEncoding("UTF-8");  
             PrintWriter out = response.getWriter();  
             String callback = ServletActionContext.getRequest().getParameter("CKEditorFuncNum");    
             out.println("<script type=\"text/javascript\">");    
             out.println("window.parent.CKEDITOR.tools.callFunction(" + callback + ",'" + "/articleImage/"+String.valueOf(article_ID)+"/" + fileName + "','')");    
             out.println("</script>");  
-            System.err.print("成功");
+            System.err.printf("成功");
             return null;
         } catch (FileNotFoundException ex) {
             Logger.getLogger(UploadAction.class.getName()).log(Level.SEVERE, null, ex);
