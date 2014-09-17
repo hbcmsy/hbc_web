97f5edaeff7fec48b48f792e78f1463e60f3ed3a
yangyaofei <yangyaofei@gmail.com>
2014-09-17 16:43:19 +0800

    url rewirte
    公告显示
    
    Signed-off-by: yangyaofei <yangyaofei@gmail.com>

--- src/action/ShowAction.java
+++ src/action/ShowAction.java
@@ -6,9 +6,14 @@
 
 package action;
 
+import java.util.List;
+
 import com.opensymphony.xwork2.ActionSupport;
 
+import common.Evn;
+import common.PageList;
 import model.Article;
+import model.Title;
 import service.ArticleService;
 
 /**
@@ -19,13 +24,39 @@ import service.ArticleService;
 public class ShowAction extends ActionSupport{
     int article_ID;
     Article article;
+    
+    PageList<Title> plist;
+    int pageNo=1;
     public String show(){
         article = new ArticleService().getAreticle(article_ID);
-        System.out.print("查看 action.showAction");
+        //System.out.print("查看 action.showAction");
         return "show";
     }
+    public String info(){
+        article = new ArticleService().getAreticle(article_ID);
+        //System.out.print("查看 action.showAction");
+        return "info";
+    }
+    public String listInfo(){
+    	List<Title> list = new ArticleService().getArticleList('a',-1,"g",Evn.ARTICLE.USE);
+    	plist = new PageList<>(list,list.size(),10,pageNo,"hehe");
+    	return "listInfo";
+    }
+    
 
-    public int getArticle_ID() {
+    public PageList<Title> getPlist() {
+		return plist;
+	}
+	public void setPlist(PageList<Title> plist) {
+		this.plist = plist;
+	}
+	public int getPageNo() {
+		return pageNo;
+	}
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+	public int getArticle_ID() {
         return article_ID;
     }
 
